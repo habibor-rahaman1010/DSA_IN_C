b5b587eabd3f260f92465e1bb3a7e67f1656e3a8
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-07 14:00:09 +0600

    hello programmer

--- sort_algorithms/bubble_sort/program-2.c
+++ sort_algorithms/bubble_sort/program-2.c
@@ -6,7 +6,7 @@ void bubble_sort(int ara[], int n){
     int temp;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n - i - 1; j++){
-            if(ara[j] > ara[j + 1]){
+            if(ara[j] < ara[j + 1]){
                 temp = ara[j];
                 ara[j] = ara[j + 1];
                 ara[j + 1] = temp;
@@ -25,3 +25,4 @@ int main() {
 
 return 0;
 }
+
