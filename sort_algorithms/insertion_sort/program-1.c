01876afc9dd6a786ce5c68b002976c50ec410a13
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-11 11:22:01 +0600

    hello

---
+++ sort_algorithms/insertion_sort/program-1.c
@@ -0,0 +1,29 @@
+//insertion sort in c program ascending order...
+
+#include <stdio.h>
+
+void insertion_sort(int ara[], int n){
+    int i, j, item;
+    for(i = 1; i < n; i++){
+        item = ara[i];
+
+        j = i - 1;
+        while(j >= 0 && ara[j] > item){
+            ara[j + 1] = ara[j];
+            j = j - 1;
+        }
+        ara[j + 1] =item;
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", ara[i]);
+    }
+}
+
+int main(){
+    int ara[] = {12, 7, 2, 3, 14, 1};
+    int length = sizeof(ara) / sizeof(ara[0]);
+    insertion_sort(ara, length);
+
+return 0;
+}
