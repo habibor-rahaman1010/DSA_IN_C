03dddeaafd5b3019f52eeb8c1283e5acc7b383b3
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-28 14:29:38 +0600

    ok done

--- linked_list/program-1.c
+++ linked_list/program-1.c
@@ -21,10 +21,25 @@ Node *createNewNode(Node *next, int item){
     return newNode;
 }
 
-int main() {
-    Node *n;
-    
+//this is my prepend function...
+Node *prepend(Node *head, int item){
+    Node *newNode = createNewNode(head, item);
+    return newNode;
+}
 
+int main() {
+    Node *n1, *n2, *head, *n3, *n4;
+    n1 = createNewNode(NULL, 10);
+    head = n1;
+    head = prepend(head, 20);
+    head = prepend(head, 30);
+
+    n2 = head;
+    n3 = n2->next;
+    n4 = n3->next;
+    printf("%d ", n2->data);
+    printf("%d ", n3->data);
+    printf("%d ", n4->data);
 
 return 0;
 }
