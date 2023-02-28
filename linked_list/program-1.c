f5a53a678321fc8211778b1b5ff2b72c9c62b962
habibor-rahaman1010 <habibor144369@gmail.com>
2023-02-28 14:06:09 +0600

    ok

--- linked_list/program-1.c
+++ linked_list/program-1.c
@@ -1,1 +1,30 @@
 //linked list implement in c program...
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
+struct node{
+    int data;
+    Node *next;
+};
+
+//create a new node...
+Node *createNewNode(Node *next, int item){
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    if(newNode == NULL){
+        printf("Error! could noed create node try again \n");
+        exit(1);
+    }
+    newNode->data = item;
+    newNode->next = next;
+    return newNode;
+}
+
+int main() {
+    Node *n;
+    
+
+
+return 0;
+}
