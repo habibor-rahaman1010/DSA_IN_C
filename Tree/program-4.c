913fa5f8bb1be865ff4e2607c21ef3f82b78e753
habibor-rahaman1010 <habibor144369@gmail.com>
2023-03-06 15:46:39 +0600

    Hello programer

---
+++ Tree/program-4.c
@@ -0,0 +1,76 @@
+//Three types of tree traversal in c program...
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
+struct node{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *CreateNewNode(int item){
+    Node *new_node = (Node *) malloc(sizeof(Node));
+    new_node->data = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+void Add_Left_Child(Node *node, Node *child){
+    node->left = child;
+}
+
+void Add_Right_Child(Node *node, Node *child){
+    node->right = child;
+}
+
+Node *Create_Tree(){
+    Node *two = CreateNewNode(2);
+    Node *seven = CreateNewNode(7);
+    Node *nine = CreateNewNode(9);
+    Add_Left_Child(two, seven);
+    Add_Right_Child(two, nine);
+
+    Node *one = CreateNewNode(1);
+    Node *six = CreateNewNode(6);
+    Add_Left_Child(seven, one);
+    Add_Right_Child(seven, six);
+
+    Node *five = CreateNewNode(5);
+    Node *ten = CreateNewNode(10);
+    Add_Left_Child(six, five);
+    Add_Right_Child(six, ten);
+
+    Node *eight = CreateNewNode(8);
+    Add_Right_Child(nine, eight);
+
+    Node *three = CreateNewNode(3);
+    Node *four = CreateNewNode(4);
+    Add_Left_Child(eight, three);
+    Add_Right_Child(eight, four);
+
+    return two;
+}
+
+//pre order traversal...
+void in_order(Node *root){
+    if(root->left != NULL){
+        in_order(root->left);
+    }
+    printf("%d ", root->data);
+
+    if(root->right != NULL){
+        in_order(root->right);
+    }
+
+}
+
+int main(){
+    Node *root = Create_Tree();
+    in_order(root);
+
+return 0;
+}
+
